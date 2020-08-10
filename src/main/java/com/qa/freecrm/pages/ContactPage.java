ead6ec3e9a40290f268adf6dc307ae6d143f7a31
Muhammed.Hashin <Muhammed.Hashin@contractingplus.com>
2020-08-10 16:26:19 +0530

    added suite file

--- src/main/java/com/qa/freecrm/pages/ContactPage.java
+++ src/main/java/com/qa/freecrm/pages/ContactPage.java
@@ -1,27 +1,23 @@
+/**
+ * 
+ */
 package com.qa.freecrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.freecrm.util.ElementUtil;
 
 /**
  * @author Muhammed.Hashin
  *
  */
-import org.openqa.selenium.WebDriver;
+public class ContactPage {
 
-import com.qa.freecrm.util.ElementUtil;
-
-public class HomePage {
-	
 	private WebDriver driver;
 	ElementUtil elementUtil;
 	
-	//locators
-	
-	//constructor of home page
-	public HomePage(WebDriver driver) {
+	public ContactPage(WebDriver driver) {
 		this.driver=driver;
 		elementUtil=new ElementUtil(this.driver);
 	}
-	
-	
-	
-
 }
